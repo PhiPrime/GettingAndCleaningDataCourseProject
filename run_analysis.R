cb81be4cef515b21b740e08625dde00ec95c204b
PhiPrime <phiprime1618@gmail.com>
2020-04-04 17:58:52 -0400

    run_analysis function written and output included

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 run_analysis <- function() {
-##Step 0.1: Configure working directory
+##Step 0.1: Check for UCI directory & create path names
 
         #Check for UCI directory
         if (!dir.exists("UCI HAR Dataset")) {
@@ -10,9 +10,13 @@ run_analysis <- function() {
         testDir  <- paste0(getwd(), "/UCI HAR Dataset/test")
         trainDir <- paste0(getwd(), "/UCI HAR Dataset/train")
 
-##Step 0.2: Load in the "train" and the "test" data sets
+##Step 0.2: Load plyr & dplyr packages
+        library(plyr)
+        library(dplyr)
 
-        #Returns names of Inertial folder files
+##Step 0.3: Load in the "train" and the "test" data sets
+
+        #This function returns the names of Inertial folder files
         getInertialPaths <- function(dir) {
                 paths <- dir(paste0(dir, "/Inertial Signals"),
                              pattern = "\\.txt", full.names = TRUE)
@@ -23,7 +27,7 @@ run_analysis <- function() {
                 paths
         }
 
-        #Handling files outside of Inertial folder
+        #This function handles files outside of Inertial folder
         getFixedVars <- function(dir) {
 
 
@@ -41,33 +45,35 @@ run_analysis <- function() {
                 #Create a row for each type of inertial observation
                 inertialObservationNames <- names(getInertialPaths(dir))
 
-                library(dplyr)
                 compiledFixed <- slice(rawFixed,
                                    rep(1:nrow(rawFixed),
                                        times = length(inertialObservationNames)))
 
+                observation_type <-  rep(inertialObservationNames,
+                                       each = nrow(rawFixed))
+
+                #split variable for observation and test/train group
+                splitNames <- strsplit(sub("_[t]", ".t", observation_type)
+                                       , "\\.")
+
+                observation_type <- sapply(splitNames, function(x){x[[1]]})
+                group <- sapply(splitNames, function(x){x[[2]]})
+
                 #Return data.frame of variables
-                data.frame(compiledFixed,
-                           observation_type = rep(inertialObservationNames,
-                                                  each = nrow(rawFixed)))
+                data.frame(group,
+                           compiledFixed,
+                           observation_type)
 
         }
 
-        #Handling files in Inertial folder
+        #This function handles files in Inertial folder
         getMeasuredVars <- function(dir) {
 
                 paths <- getInertialPaths(dir)
-
-                getObservation <- function(rownum) {
-                        sapply(paths, read.table,
-                               skip = (rownum - 1), nrows = 1)
-                }
-
-
-                # library(plyr)
                 data <- ldply(paths, read.table, stringsAsFactors = FALSE)
-                data[,2:length(data[1,])]
 
+                #Remove filename label since getFixedVars() includes it
+                data[,2:length(data[1,])]
         }
 
 
@@ -81,21 +87,58 @@ run_analysis <- function() {
 
 ##Step 1: Merge the training and the test sets to create one data set.
 
+        mergedData <- arrange(rbind(testData,trainData),
+                              subject_id, activity_id)
+        mergedData
+
 
 ##Step 2: Extract only the measurements on the mean and
 ##              standard deviation for each measurement.
 
+        means <-  sapply(1:length(mergedData[,1]),
+                         function(x){mean(
+                                 as.numeric(as.character(
+                                         mergedData[x,5:length(mergedData[1,])]
+                                         )))})
+        stdDevs <- sapply(1:length(mergedData[,1]),
+                          function(x){sd(
+                                  as.numeric(as.character(
+                                          mergedData[x,5:length(mergedData[1,])]
+                                  )))})
+        consolidatedData <- data.frame(mergedData[,1:4], #1:4 are the fixed vars
+                                       mean = means,
+                                       standard_deviation = stdDevs)
+
 
 ##Step 3: Use descriptive activity names to name the activities in the data set
 
+        #Uses activity_labels file for descriptions of activity_id
+        activityDir <- paste0(getwd(), "/UCI HAR Dataset/activity_labels.txt")
+        activityLabels <- read.table(activityDir)
+        consolidatedData <- mutate(consolidatedData,
+                             activity_id = activityLabels[activity_id,2])
+
 
 ##Step 4: Appropriately label the data set with descriptive variable names.
+
+        ##consolidatedData currently has colNames:
+        ##      group, subject_id, activity_id, observation_type, mean,
+        ##                                      & standard_deviation
+        ##Since activity_id is now the name of an activity that's the
+        ##only one that needs renamed
+
+        consolidatedData <- rename(consolidatedData, c("activity_id" = "activity"))
 
 
 ##Step 5: From the data set in step 4, create a second,
 ##              independent tidy data set with the average of each variable
 ##              for each activity and each subject.
 
-}
+        summary <- consolidatedData %>%
+                ddply(.(group, subject_id, activity, observation_type),
+                      summarize,
+                      mean = mean(mean), mean_of_sd = mean(standard_deviation))
 
-boi <- run_analysis()
+        write.csv(summary, "./UCI_HAR_Dataset_Summary.csv")
+}
+run_analysis()
